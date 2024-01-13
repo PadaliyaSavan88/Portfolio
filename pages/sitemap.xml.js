4100a3def3711f74809edd58e53dadc33da886c5
Savan Padaliya <padaliyasavan8@gmail.com>
2024-01-13 11:28:11 +0530

    sitemap updated and canocical ur added to blogs

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -5,7 +5,7 @@ function generateSiteMap(posts) {
     return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
    <url>
-       <loc>https://www.theblockchaininsider.com</loc>
+       <loc>https://www.theblockchaininsider.com/</loc>
      </url>
      <url>
        <loc>https://www.theblockchaininsider.com/allBlogs</loc>
