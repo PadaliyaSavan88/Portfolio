c069dd371347ad0321b530ee0d7e2463da5df8de
Savan Padaliya <padaliyasavan8@gmail.com>
2024-01-16 12:51:08 +0530

    _app Head import issue fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,27 @@
 // import '@/styles/globals.css'
 // import '../public/Assests/js/main'
 
+import Head from "next/head";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="robots" content="all" /></Head>
       {/* <Script src="./Assests/js/script.js"/> */}
       <Script async strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-N3GKFN94TH"></Script>
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag() { dataLayer.push(arguments) }
           gtag('js', new Date());
           
           gtag('config', 'G-N3GKFN94TH');
         `}
-        </Script>
-        <link rel="stylesheet" href="/Assests/Css/bootstarp.css" />
-        <Head>
-        <meta name="robots" content="all" /></Head>
+      </Script>
+      <link rel="stylesheet" href="/Assests/Css/bootstarp.css" />
+
       <Component {...pageProps} />
     </>
   )
