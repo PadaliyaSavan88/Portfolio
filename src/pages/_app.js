1871c1d9ee460075799e9eb60af56135946a7bd0
PadaliyaSavan88 <padaliyasavan8@gmail.com>
2026-04-17 10:21:54 +0530

    feat: google analytics added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,10 @@
 import { useEffect } from 'react';
 import '../styles/globals.css'
 import Head from 'next/head'
+import Script from 'next/script'
 import 'bootstrap/dist/css/bootstrap.css';
+
+const GA_ID = 'G-YBNML3W5EP';
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -9,8 +12,21 @@ export default function App({ Component, pageProps }) {
   }, [])
   return (
     <>
-    <Head>
-        <meta name="robots" content="all" /></Head>
+      <Head>
+        <meta name="robots" content="all" />
+      </Head>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   )
